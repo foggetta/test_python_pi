c69dba5c4051703b3d38223b41cc9eb5735b44ed
foggetta <foggetta@gmail.com>
2017-12-30 12:54:43 +0100

    modificate lee funzioni in weather .py

--- weather.py
+++ weather.py
@@ -1,14 +1,15 @@
+#test weather
 from requests import get
 import json
 from pprint import pprint
 from haversine import haversine
-stations = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallstations'
-weather = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/'
-my_lat = 52.194504
-my_lon = 0.134708
-all_stations = get(stations).json()['items']
 
-def find_closest():
+def put_stations():
+    stations = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getallstations'
+    all_stations = get(stations).json()['items']
+    return all_stations
+    
+def find_closest(all_stations, my_lon, my_lat):
     smallest = 20036
     for station in all_stations:
         station_lon = station['weather_stn_long']
@@ -18,7 +19,11 @@ def find_closest():
             smallest = distance
             closest_station = station['weather_stn_id']
     return closest_station
-closest_stn = find_closest()
-weather_cls = weather + str(closest_stn)
-my_weather = get(weather_cls).json()['items']
-pprint(my_weather)
+
+def get_weather(my_lon, my_lat):
+    weather = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/'
+    closest_stn = find_closest(put_stations(), my_lon, my_lat)
+    weather_cls = weather + str(closest_stn)
+    my_weather = get(weather_cls).json()['items']
+    return my_weather
+    
